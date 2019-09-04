f9c273e06a8be319fb85013f7358bbe3ae29ff65
Luigi Leoni <luigi.leoni@sorint.it>
2019-09-04 16:48:51 +0200

    Added shutdown listener

--- index.js
+++ index.js
@@ -29,7 +29,27 @@ app.get('/', async function(req, res) {
     })
 });
 
-let server = app.listen(8080, function() {
+let server = app.listen(8080,'0.0.0.0', function() {
     console.log('Server is listening on port 8080')
 });
 
+var signals = {
+  'SIGHUP': 1,
+  'SIGINT': 2,
+  'SIGTERM': 15
+};
+
+const shutdown = (signal, value) => {
+  console.log("shutdown!");
+  server.close(() => {
+    console.log(`server stopped by ${signal} with value ${value}`);
+    process.exit(128 + value);
+  });
+};
+
+Object.keys(signals).forEach((signal) => {
+  process.on(signal, () => {
+    console.log(`process received a ${signal} signal`);
+    shutdown(signal, signals[signal]);
+  });
+});
